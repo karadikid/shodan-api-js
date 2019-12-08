3f8d1b10172db60ab61853166ae7b8dfbdc18df7
Gouthum Karadi <goukaradi@hotmail.com>
2019-12-07 22:22:06 -0500

    Adding screenshot

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log("Script Loaded");
 //https://api.shodan.io/shodan/host/search?key=6Ew4Z6p9kOwelROHLtOwn23ChCIoR0H1&query=port:1494,3389
 
 const url = "https://api.shodan.io/shodan/host/search?key=";
-const apiKey = "uBSyQy4PYkeknFcBrqBdsdLKmWFo7PFv";
+const apiKey = "";
 const query = "&query=port:1494,3389 org:amazon web services";
 const facets = "";
 
